f829e20d9261190051d2cd2b102b32057a3ff3b8
Niranjan Devasani <niranjan.devasani@gmail.com>
2023-09-21 08:53:00 +0530

    Website

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import {NLink, NButton, NTheme, useLocalStorage, THEMES} from 'nayan';
 import { MoonStar, Sun } from 'lucide-react';
+import Banner from "./components/Banner";
 
 const App = () => {
   const [theme, setTheme] = useLocalStorage('THEME', THEMES.LIGHT);
@@ -9,9 +10,11 @@ const App = () => {
   };
   return (
     <NTheme theme={theme}>
-    <div className="h-screen w-screen items-center p-5">
-      <div className="container grid grid-cols-1 md:grid-cols-2 gap-3 h-full">
-        <div className="flex flex-col h-full">
+    <div className="h-screen w-screen items-center">
+      <div className="container mx-auto h-full">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 h-full">
+        <div className="flex items-center">
+        <div className="p-5">
           <div className="text-4xl mb-5">Hello Guys 👋.</div>
           <div className="text-xl leading-relaxed mb-5">I'm <NLink>Niranjan Devasani.</NLink> Web and Mobile application developer, Cloud and Devops Engineer, Opensource Contributor and Creator of <a href="https://nayanui.com" target="_blank" className="text-primary">Nayan UI 🔥</a>.</div>
           <div className="flex flex-row">
@@ -25,6 +28,13 @@ const App = () => {
             </span>
           </div>
         </div>
+        </div>
+        <div className="flex items-center">
+        <div className=" mx-auto">
+          <Banner />
+        </div>
+        </div>
+      </div>
       </div>
     </div>
     </NTheme>
