622b698723978496eb2f86edbfb6b919b39a0cf5
Niranjan Devasani <niranjan.devasani@gmail.com>
2023-09-21 20:02:21 +0530

    Website

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -2,15 +2,19 @@ const Details = () => {
   return (
     <div className="p-3">
       <div className="text-lg font-bold mb-3">Education</div>
-      <div className="mb-5">Batchelors Technology in <strong>Computer Science & Engineering</strong> from <strong>JNTU University, Anantapur</strong> with <strong>80%</strong> Aggregate.</div>
+      <div className="mb-5">
+        <ul className="list-disc pl-5 leading-relaxed">
+          <li>Batchelors Technology in <strong>Computer Science & Engineering</strong> from <strong>JNTU University, Anantapur</strong> with <strong>80%</strong> Aggregate.</li>
+        </ul>
+      </div>
       <div className="text-lg font-bold mb-3">Work Experience</div>
       <div className="mb-5">
-        <ui>
+        <ul className="list-disc pl-5 leading-relaxed">
           <li>Currently working with <strong>Cisco</strong> from April 2021 as <strong>SDE-4</strong>.</li>
           <li>Previously worked with <strong>Wellsfargo</strong> from October 2020 to April 2021 as <strong>Assistant Vice President</strong>.</li>
           <li>Previously worked for <strong>HCL Technologies</strong> from sep 2017 to October 2020 as <strong>Technical Lead</strong>.</li>
           <li>Previously worked for <strong>Purplefront technologies</strong> from April 2014 to Sep 2017 as <strong>Senior Software engineer</strong>.</li>
-        </ui>
+        </ul>
       </div>
     </div>
   );
